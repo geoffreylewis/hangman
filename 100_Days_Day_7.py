7f29e7df39d2dcd5477f61b7f3b277a7d440469e
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-09 15:20:45 -0500

    Add files via upload
    Starting project...

---
+++ 100_Days_Day_7.py
@@ -0,0 +1,12 @@
+###########
+# Hangman #
+###########
+
+# Start of project.
+print("Welcome to the \"Hangman\" game!")
+print("You have to guess the mystery word one letter at a time.")
+print("If you guess wrong 6 times before revealing the full word, your stick figure man is hanged on the gallows.")
+print("Don't guess wrong, or it will be too late for your stick figure man!")
+
+# End of project.
+print("You win!  You saved your stick figure man!")
