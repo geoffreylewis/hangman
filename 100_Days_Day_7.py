e4fa737553f18f36ca276011ce18d0889a73fad3
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-13 20:39:16 -0500

    Add files via upload
    Added some counters and "if" statement for wrong guesses.

--- 100_Days_Day_7.py
+++ 100_Days_Day_7.py
@@ -5,6 +5,70 @@
 # Importing module.
 import random
 
+# ASCII art for the hanged man.
+hanged_man_stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+# Counter for number of body parts that appear before the stick figure man is hung.
+number_of_body_parts = 0
+
+# Counter for different ASCII images of the hanged man.
+hanged_man_stage_number = 6
+
 # Beginning of game.
 print("Welcome to the \"Hangman\" game!")
 print("You have to guess the mystery word one letter at a time.")
@@ -29,10 +93,11 @@ for char in selected_word:
 print(hidden_word_underscores) # TESTING CODE
 
 # Guessing letters.
-while "_" in hidden_word_underscores:
+while "_" in hidden_word_underscores and number_of_body_parts < 6:
+    print(hanged_man_stages[hanged_man_stage_number])
     player_guess = str(input("Guess a letter: ").lower())
-    letter_position = 0
-    for letter in selected_word:
+    letter_position = 0 # Starting position in hidden word list.
+    for letter in selected_word: # Analyzes player's guess against the selected word.
         if player_guess == letter:
             print("Right") # TESTING CODE
             hidden_word_underscores[letter_position] = letter
@@ -40,7 +105,14 @@ while "_" in hidden_word_underscores:
         else:
             letter_position += 1
             print("Wrong") # TESTING CODE
+    if player_guess not in selected_word: # Appearance of body parts / wrong guesses.
+        number_of_body_parts += 1
+        hanged_man_stage_number -= 1
     print(hidden_word_underscores) # TESTING CODE
 
 # End of project.
-print("You win!  You saved your stick figure man!")
+if "_" not in hidden_word_underscores and number_of_body_parts < 6:
+    print("You win!  You saved your stick figure man!")
+else:
+    print(hanged_man_stages[0])
+    print("You lose!  You're too late and didn't save your stick figure man!")
