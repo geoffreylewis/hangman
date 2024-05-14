1943c6cc5d7f3a02c04c4a89b82ac4697ec52103
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-14 16:26:32 -0500

    Add files via upload
    Added banner logo, re-arranged some code, deleted various test code, and added additional "if/elif" statements.

--- 100_Days_Day_7.py
+++ 100_Days_Day_7.py
@@ -5,7 +5,18 @@
 # Importing module.
 import random
 
-# ASCII art for the hanged man.
+# ASCII art for beginning banner/logo (courtesy of https://replit.com/@appbrewery/Day-7-Hangman-5-Start#hangman_art.py).
+banner_logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+# ASCII art for the hanged man (courtesy of https://replit.com/@appbrewery/Day-7-Hangman-5-Start#hangman_art.py).
 hanged_man_stages = ['''
   +---+
   |   |
@@ -63,34 +74,38 @@ hanged_man_stages = ['''
 =========
 ''']
 
+# Beginning of game.
+print(banner_logo)
+print()
+print("Welcome to the \"Hangman\" game!\n")
+print("You have to guess the mystery word one letter at a time.\n")
+print("If you guess wrong 6 times before revealing the full word, your stick figure man is hanged on the gallows.\n")
+print("Don't guess wrong, or it will be too late for your stick figure man!\n")
+
 # Counter for number of body parts that appear before the stick figure man is hung.
 number_of_body_parts = 0
 
 # Counter for different ASCII images of the hanged man.
 hanged_man_stage_number = 6
 
-# Beginning of game.
-print("Welcome to the \"Hangman\" game!")
-print("You have to guess the mystery word one letter at a time.")
-print("If you guess wrong 6 times before revealing the full word, your stick figure man is hanged on the gallows.")
-print("Don't guess wrong, or it will be too late for your stick figure man!")
-
 # Word list to pick a random word from.
 list_of_words = ["abruptly", "absurd", "abyss", "avenue", "awkward", "bagpipes", "bandwagon", "banjo", "bayou", 
-                 "bikini", "bookworm", "crypt", "dwarves", "embezzle", "funny", "galvanize", "glyph", "hyphen", 
-                 "ivory", "jigsaw", "keyhole", "lucky", "matrix", "nymph", "onyx", "phlegm", "quips", "rhythm", 
-                 "swivel", "topaz", "uptown", "vampire", "vaporize", "wellspring", "werewolf", "xylophone", 
-                 "yummy", "zombie"]
+                 "bikini", "bookworm", "chicken", "crypt", "dwarves", "embezzle", "funny", "galvanize", "goblins", "glyph", "hyphen", 
+                 "ivory", "jigsaw", "keyhole", "lucky", "matrix", "monkey", "nymph", "onyx", "phlegm", "quips", "rhythm", "royalty", 
+                 "steak", "swivel", "trolls", "topaz", "uptown", "vampire", "vaporize", "wellspring", "werewolf", "xylophone", 
+                 "yesterday", "yummy", "zebra", "zombie"]
 
 # Random word from the list is selected.
 selected_word = random.choice(list_of_words)
-print(selected_word) # TESTING CODE
 
 # Empty list for eventual "_" characters.
 hidden_word_underscores = []
 for char in selected_word:
     hidden_word_underscores.append("_")
-print(hidden_word_underscores) # TESTING CODE
+print(hidden_word_underscores)
+
+# Empty list for already-guessed letters.
+already_guessed = []
 
 # Guessing letters.
 while "_" in hidden_word_underscores and number_of_body_parts < 6:
@@ -99,20 +114,34 @@ while "_" in hidden_word_underscores and number_of_body_parts < 6:
     letter_position = 0 # Starting position in hidden word list.
     for letter in selected_word: # Analyzes player's guess against the selected word.
         if player_guess == letter:
-            print("Right") # TESTING CODE
             hidden_word_underscores[letter_position] = letter
             letter_position += 1
         else:
             letter_position += 1
-            print("Wrong") # TESTING CODE
     if player_guess not in selected_word: # Appearance of body parts / wrong guesses.
-        number_of_body_parts += 1
-        hanged_man_stage_number -= 1
-    print(hidden_word_underscores) # TESTING CODE
+        if player_guess not in already_guessed:
+            already_guessed.append(player_guess)
+            number_of_body_parts += 1
+            hanged_man_stage_number -= 1
+            print()
+            print(f"Letter \"{player_guess}\" isn't in the word.  Your stick figure man is getting closer to death.\n")
+            print("Tread lightly.")
+        elif player_guess in already_guessed:
+            print()
+            print("You already guessed this letter; pick a different one.")
+    elif player_guess in selected_word:
+        if player_guess not in already_guessed:
+            already_guessed.append(player_guess)
+        elif player_guess in already_guessed:
+            print()
+            print("You already guessed this letter; pick a different one.")
+    print()
+    print(hidden_word_underscores)
 
-# End of project.
+# End of game.
 if "_" not in hidden_word_underscores and number_of_body_parts < 6:
-    print("You win!  You saved your stick figure man!")
+    print()
+    print(f"You win!  The word was \"{selected_word}\".  You saved your stick figure man from the gallows!")
 else:
     print(hanged_man_stages[0])
-    print("You lose!  You're too late and didn't save your stick figure man!")
+    print(f"You lose!  The word was \"{selected_word}\".  You're too late and didn't save your stick figure man!")
